ceef90dc453da4f55af04741d28fe79b01e11e76
Jacek Galowicz <jacek@galowicz.de>
2024-03-10 11:02:00 +0000

    Refactor

--- flake.nix
+++ flake.nix
@@ -3,43 +3,43 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, pkgs, system, ... }: {
-        packages = {
-          minisha256sum = pkgs.callPackage ./package.nix { };
-          default = config.packages.minisha256sum;
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    perSystem = { config, pkgs, system, ... }: {
+      packages = {
+        minisha256sum = pkgs.callPackage ./package.nix { };
+        default = config.packages.minisha256sum;
 
-          x86 = pkgs.pkgsCross.gnu64.callPackage ./package.nix { };
-          x86-static = pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
-          aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
-          aarch64-static = pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage ./package.nix { };
+        x86 = pkgs.pkgsCross.gnu64.callPackage ./package.nix { };
+        x86-static = pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
+        aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
+        aarch64-static = pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage ./package.nix { };
 
-          windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
-        };
+        windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
+      };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ config.packages.default ];
-          inherit (config.checks.pre-commit-check) shellHook;
-        };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ config.packages.default ];
+        inherit (config.checks.pre-commit-check) shellHook;
+      };
 
-        checks = {
-          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              clang-format.enable = true;
-              cmake-format.enable = true;
-              deadnix.enable = true;
-              nixpkgs-fmt.enable = true;
-              statix.enable = true;
-            };
+      checks = {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            clang-format.enable = true;
+            cmake-format.enable = true;
+            deadnix.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
           };
         };
       };
-      flake = { };
     };
+  };
 }
