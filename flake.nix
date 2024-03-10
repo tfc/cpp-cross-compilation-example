90bfeb11ffa9456db989e833314fea9d23bb9f02
Jacek Galowicz <jacek@galowicz.de>
2024-03-10 11:01:14 +0000

    Provide minimized shell to demo compiler switch

--- flake.nix
+++ flake.nix
@@ -9,43 +9,23 @@
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-    perSystem = { config, pkgs, system, ... }: {
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    perSystem = { config, pkgs, ... }: {
+
       packages = {
-        minisha256sum = pkgs.callPackage ./nix/package.nix { };
-        default = config.packages.minisha256sum;
-        clang = config.packages.minisha256sum.override {
-          stdenv = pkgs.clangStdenv;
-        };
-        static = pkgs.pkgsStatic.callPackage ./nix/package.nix { };
-
-        x86 = pkgs.pkgsCross.gnu64.callPackage ./nix/package.nix { };
-        x86-static = pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./nix/package.nix { };
-        aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./nix/package.nix { };
-        aarch64-static = pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage ./nix/package.nix { };
-
-        windows = pkgs.pkgsCross.mingwW64.callPackage ./nix/package.nix { };
-
-        inherit ((pkgs.nixos [ ./nix/config-app.nix ./nix/config-vm.nix ]).config.system.build) vm;
+        default = pkgs.callPackage ./nix/package.nix { };
       };
 
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [ config.packages.default ];
-        inherit (config.checks.pre-commit-check) shellHook;
+      devShells = {
+        inherit (config.packages) default;
+        clang = config.packages.default.override { stdenv = pkgs.clangStdenv; };
       };
 
-      checks = {
-        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            clang-format.enable = true;
-            cmake-format.enable = true;
-            deadnix.enable = true;
-            nixpkgs-fmt.enable = true;
-            statix.enable = true;
-          };
-        };
-      } // config.packages;
     };
   };
 }
